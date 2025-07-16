cb59482fb863639e7bb2200a70694ea124f9bd48
akagikouzanh <nagaoka041.work@gmail.com>
2025-07-16 20:36:13 +0900

    add setuptools_scm

--- hellopkg/__init__.py
+++ hellopkg/__init__.py
@@ -1,1 +1,6 @@
-__version__ = '1.0.0'
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("hellopkg")
+except PackageNotFoundError:
+    __version__ = "unknown"
--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup, find_packages
 
-version_ns = {}
-with open("hellopkg/__init__.py") as f:
-    exec(f.read(),version_ns)
-
-
 setup(
     name="hellopkg",
-    version=version_ns["__version__"],
+    # ここでカスタムversion_scheme、local_schemeを記載するのであれば、toml側では不要
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     packages=find_packages(),
     entry_points={"console_scripts": ["hello=hellopkg.cli:main",]},
 )
