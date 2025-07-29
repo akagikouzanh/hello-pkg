6043f9f4b8805ae772d8b640ae677201d0fc0c78
akagikouzanh <nagaoka041.work@gmail.com>
2025-07-29 11:00:43 +0900

    update version format

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 from setuptools_scm import get_version
 
 target_version = get_version()
-fromatted_version = target_version.replace("dev-v", "").replace('v', '') if target_version else None
+formatted_version = target_version.replace('v', '') if target_version else None
 
 setup(
     name="hellopkg",
-    version=fromatted_version,
+    version=formatted_version,
     setup_requires=["setuptools_scm"],
     packages=find_packages(),
     entry_points={"console_scripts": ["hello=hellopkg.cli:main",]},
