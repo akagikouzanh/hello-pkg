7eac882826500bbc98c4c3c9ea02fdac3ed1c1ab
akagikouzanh <nagaoka041.work@gmail.com>
2025-07-29 10:25:18 +0900

    set get_version

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
+from setuptools_scm import get_version
+
 
 setup(
     name="hellopkg",
     # ここでカスタムversion_scheme、local_schemeを記載するのであれば、toml側では不要
-    use_scm_version=True,
+    version=get_version(),
     setup_requires=["setuptools_scm"],
     packages=find_packages(),
     entry_points={"console_scripts": ["hello=hellopkg.cli:main",]},
