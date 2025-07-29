4d046e8dad6cd2c804e144929917e57294aef67e
akagikouzanh <nagaoka041.work@gmail.com>
2025-07-29 10:42:37 +0900

    fix set version setting

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from setuptools import setup, find_packages
+from setuptools_scm import get_version
 
-def format_version(version):
-    return version.tag.replace("dev-v", "").replace('v', '') if version.tag else None
+target_version = get_version()
+fromatted_version = target_version.replace("dev-v", "").replace('v', '') if target_version else None
 
 setup(
     name="hellopkg",
-    # ここでカスタムversion_scheme、local_schemeを記載するのであれば、toml側では不要
-    use_scm_version={
-        "version_scheme": format_version,
-        "local_scheme": "no-local-version",
-    },
+    version=fromatted_version,
     setup_requires=["setuptools_scm"],
     packages=find_packages(),
     entry_points={"console_scripts": ["hello=hellopkg.cli:main",]},
